9d3839d8d0b1204448014aa369a9a2db1240c3ef
HillsKalathiya <100057465+manishkalathiya@users.noreply.github.com>
2024-01-23 19:08:02 +0530

    Add files via upload

---
+++ ADDITION.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a=10;
+	int b=20;
+	clrscr();
+	printf("%d-%d");
+	printf("=10");
+	getch();
+
+
+}
---
+++ PROGRAM.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	int a=15;
+	clrscr();
+	printf("%d\n",++a);
+	printf("%d",--a);
+	getch();
+
+
+
+}
